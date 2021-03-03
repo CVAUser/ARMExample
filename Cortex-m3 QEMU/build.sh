eb181a47d6dda5299db7cd830f574cb634cd0c7d
CVAUser <57252726+CVAUser@users.noreply.github.com>
2021-03-03 17:32:40 +0300

    Create minimal C programm

--- Cortex-m3 QEMU/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-arm-none-eabi-as -c -mthumb -mlittle-endian -march=armv7-m -mcpu=cortex-m3 startup.S -o startup.o
-arm-none-eabi-gcc -c -mthumb -ffreestanding -mlittle-endian -march=armv7-m -mcpu=cortex-m3 test.c -o test.o
-arm-none-eabi-ld -T test.ld test.o startup.o -o test.elf
