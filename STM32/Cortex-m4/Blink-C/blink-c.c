a067a844101d7357c7e90320cc439eb08ebe4624
CVAUser <57252726+CVAUser@users.noreply.github.com>
2021-03-12 00:08:25 +0300

    blink work
    written in C language

--- STM32/Cortex-m4/Blink-C/blink-c.c
+++ STM32/Cortex-m4/Blink-C/blink-c.c
@@ -1,3 +1,22 @@
+#define RCC_AHB1ENR (*(volatile int *)(0x40023830U))
+#define GPIO_D_ENABLE (0x00000008U)
+
+#define GPIOD (*(volatile int *)(0x40020C00U))
+#define GPIO_BSSR (*(volatile int *)(0x40020C18U))
+
 void main() {
-	for (;;);
+	RCC_AHB1ENR = GPIO_D_ENABLE;
+	GPIOD = 0x01000000U;
+
+	while (1) {
+		GPIO_BSSR = 0x1000U;
+		int i;
+		for (i = 0; i < 1000000; i++) {	/* Wait a bit. */
+			__asm__("nop");
+		}
+		GPIO_BSSR = 0x10000000U;
+		for (i = 0; i < 1000000; i++) {	/* Wait a bit. */
+			__asm__("nop");
+		}
+	}
 }
