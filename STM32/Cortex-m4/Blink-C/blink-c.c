830483b7595df8216ff71ad0a5977ae7a98f7de9
CVAUser <57252726+CVAUser@users.noreply.github.com>
2021-03-14 13:13:35 +0300

    compile w/o standart startup file

--- STM32/Cortex-m4/Blink-C/blink-c.c
+++ STM32/Cortex-m4/Blink-C/blink-c.c
@@ -1,20 +1,22 @@
-#define RCC_AHB1ENR (*(volatile int *)(0x40023830U))
-#define GPIO_D_ENABLE (0x00000008U)
+#include <stdio.h>
 
-#define GPIOD (*(volatile int *)(0x40020C00U))
-#define GPIO_BSSR (*(volatile int *)(0x40020C18U))
+#define RCC_AHB1ENR (*(volatile uint32_t *)(0x40023830))
+#define GPIO_D_ENABLE (0x00000008)
+
+#define GPIOD (*(volatile uint32_t *)(0x40020C00))
+#define GPIO_BSSR (*(volatile uint32_t *)(0x40020C18))
 
 void main() {
 	RCC_AHB1ENR = GPIO_D_ENABLE;
-	GPIOD = 0x01000000U;
+	GPIOD = 0x01000000;
 
 	while (1) {
-		GPIO_BSSR = 0x1000U;
+		GPIO_BSSR = 0x1000;
 		int i;
 		for (i = 0; i < 1000000; i++) {	/* Wait a bit. */
 			__asm__("nop");
 		}
-		GPIO_BSSR = 0x10000000U;
+		GPIO_BSSR = 0x10000000;
 		for (i = 0; i < 1000000; i++) {	/* Wait a bit. */
 			__asm__("nop");
 		}
