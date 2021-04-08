960d5570e215c4e074af25c814859d7716ac1b27
CVAUser <57252726+CVAUser@users.noreply.github.com>
2021-04-08 12:08:21 +0300

    optimize blink program

--- STM32/Cortex-m4/Blink/blink.s
+++ STM32/Cortex-m4/Blink/blink.s
@@ -10,11 +10,6 @@
 
   .equ GPIO_BSSR      ,0x18
 
-  .macro  MOV32 regnum,number
-          MOVW \regnum,:lower16:\number
-          MOVT \regnum,:upper16:\number
-  .endm
-
   .text
   .thumb
   .global Reset_Exception
@@ -29,28 +24,27 @@ Reset_Exception:
     ldr r0,     =RCC_BASE + RCC_AHB1ENR
     str r1,     [r0]
 
-
     mov.w r3,   GPIOD_MODER
     ldr r2,     =GPIOD
     str r3,     [r2]
 
 BLINK_Loop:
-    MOV32 r0   ,GPIOD + GPIO_BSSR
-    MOV32 r1   ,0x1000
-    str r1     ,[r0]
+    mov.w r1,   0x1000
+    ldr r0,     =GPIOD + GPIO_BSSR
+    str r1,     [r0]
 
     bl         DELAY
 
-    MOV32 r0   ,GPIOD + GPIO_BSSR
-    MOV32 r1   ,0x10000000
-    str r1     ,[r0]
+    ldr r0,     =GPIOD + GPIO_BSSR
+    ldr r1,     =0x10000000
+    str r1,     [r0]
 
     bl         DELAY
 
     b          BLINK_Loop
 
 DELAY:
-    MOV32	r2   ,0x00100000
+    ldr	r2,    =0x00100000
 
 Delay_loop:
     subs	     r2, r2, 1
